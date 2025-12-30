caa8c0dc816a7acb5515e0f59a6e32343c63f1fa
legitedeV <legite1337@gmail.com>
2025-12-30 17:57:32 +0000

    Fix backend-v2 auth & users DTO and tests

--- backend-v2/src/users/dto/update-user.dto.ts
+++ backend-v2/src/users/dto/update-user.dto.ts
@@ -1,6 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Role } from '@prisma/client';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   password?: string;
+  firstName?: string;
+  lastName?: string;
+  role?: Role;
+  employeeId?: string;
 }
