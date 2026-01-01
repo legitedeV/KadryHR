84480db0ad4faa239a9857eba0734f237f86fe85
legitedeV <legite1337@gmail.com>
2026-01-01 17:39:22 +0000

    BACKEND UPDATE

--- backend-v2/src/prisma/prisma.service.ts
+++ backend-v2/src/prisma/prisma.service.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-=======
 import {
   Injectable,
   OnModuleInit,
@@ -17,28 +5,18 @@ import {
   INestApplication,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
->>>>>>> 30bbeef751f1e7ff4deb2abb586dd55f841071c0
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    // przekazujemy adapter; opcjonalnie logi dla wygody
-    super({
-      adapter,
-      log: ['error', 'warn'],
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
-    await pool.end();
   }
 
   async enableShutdownHooks(app: INestApplication) {
