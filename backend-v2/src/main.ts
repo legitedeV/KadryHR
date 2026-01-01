9fbfaf552907a3dcb7badab5ffcb6d43b758c1eb
legitedeV <dbgwmjv5vc@privaterelay.appleid.com>
2026-01-01 15:53:18 +0100

    Add Prisma shutdown hook and tests

--- backend-v2/src/main.ts
+++ backend-v2/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   app.useGlobalPipes(
     new ValidationPipe({
